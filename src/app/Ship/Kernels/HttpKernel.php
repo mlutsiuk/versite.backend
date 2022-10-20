95995c12540386629a0b442bb6bc08f8d293884e
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-20 04:02:30 +0300

    feat: Enabled Sanctum EnsureFrontendRequestsAreStateful global middleware

--- src/app/Ship/Kernels/HttpKernel.php
+++ src/app/Ship/Kernels/HttpKernel.php
@@ -39,7 +39,7 @@ class HttpKernel extends Kernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
