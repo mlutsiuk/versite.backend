50f3bfe1e9a19f9bd681fa2037eb1417b3f7efcc
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-22 21:12:03 +0300

    fix: Add missing GoogleLoginController to previous commit

--- src/app/Containers/AppSection/Authentication/UI/API/Controllers/GoogleLoginController.php
+++ src/app/Containers/AppSection/Authentication/UI/API/Controllers/GoogleLoginController.php
@@ -2,17 +2,41 @@
 
 namespace App\Containers\AppSection\Authentication\UI\API\Controllers;
 
+use App\Containers\AppSection\Authentication\Actions\HandleGoogleLoginCallbackAction;
+use App\Containers\AppSection\Authentication\Exceptions\UnauthenticatedException;
+use App\Containers\AppSection\Authentication\Tasks\CreateGoogleLoginRedirectUrlTask;
 use App\Ship\Parents\Controllers\ApiController;
+use Illuminate\Http\JsonResponse;
 
 class GoogleLoginController extends ApiController
 {
-    public function redirect()    // TODO
+    /**
+     * Create link to Google authentication page
+     *
+     * @return JsonResponse
+     */
+    public function redirect(): JsonResponse
     {
+        $redirectUrl = app(CreateGoogleLoginRedirectUrlTask::class)->run();
 
+        return response()->json([
+            'url' => $redirectUrl
+        ]);
     }
 
-    public function handleCallback()    // TODO
+    /**
+     * Handle Google login callback, login or create new account.
+     *
+     * @throws UnauthenticatedException
+     */
+    public function handleCallback(): JsonResponse
     {
+        $accessToken = app(HandleGoogleLoginCallbackAction::class)->run();
 
+        return $this->json([
+            'token_type' => 'Bearer',
+            'expires_in' => config('apiato.api.expires-in'),
+            'access_token' => $accessToken
+        ]);
     }
 }
