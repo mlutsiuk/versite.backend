321789ca6b62add39f2f32c5ed51332a2b96c9a7
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-24 05:34:18 +0300

    feat: Add Authorization container

--- src/app/Ship/Parents/Models/UserModel.php
+++ src/app/Ship/Parents/Models/UserModel.php
@@ -5,8 +5,11 @@ namespace App\Ship\Parents\Models;
 use Apiato\Core\Abstracts\Models\UserModel as AbstractUserModel;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 abstract class UserModel extends AbstractUserModel
 {
-    use HasApiTokens, Notifiable;
+    use HasApiTokens;
+    use HasRoles;
+    use Notifiable;
 }
