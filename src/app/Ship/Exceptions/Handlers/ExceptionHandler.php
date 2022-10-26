0d7a2415bb7f09ae27039591a8c76c88a0951c91
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-26 06:02:27 +0300

    feat: Configure ExceptionHandler. Add NotAuthorizedResourceException & NotFoundException

--- src/app/Ship/Exceptions/Handlers/ExceptionHandler.php
+++ src/app/Ship/Exceptions/Handlers/ExceptionHandler.php
@@ -2,11 +2,26 @@
 
 namespace App\Ship\Exceptions\Handlers;
 
-use Illuminate\Foundation\Exceptions\Handler;
+use Apiato\Core\Abstracts\Exceptions\Exception as CoreException;
+use Apiato\Core\Exceptions\AuthenticationException as CoreAuthenticationException;
+use Apiato\Core\Exceptions\Handlers\ExceptionsHandler as CoreExceptionsHandler;
+use App\Ship\Exceptions\NotAuthorizedResourceException;
+use App\Ship\Exceptions\NotFoundException;
+use App\Ship\Providers\RouteServiceProvider;
+use Illuminate\Auth\AuthenticationException as LaravelAuthenticationException;
+use Illuminate\Http\JsonResponse;
 use Psr\Log\LogLevel;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
-class ExceptionHandler extends Handler
+/**
+ * Class ExceptionsHandler
+ *
+ * A.K.A. (app/Exceptions/Handler.php)
+ */
+class ExceptionHandler extends CoreExceptionsHandler
 {
     /**
      * A list of exception types with their corresponding custom log levels.
@@ -27,7 +42,7 @@ class ExceptionHandler extends Handler
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array<int, string>
      */
@@ -44,8 +59,49 @@ class ExceptionHandler extends Handler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->reportable(static function (Throwable $e) {
         });
+
+        $this->renderable(function (CoreException $e) {
+            return $this->buildResponse($e);
+        });
+
+        $this->renderable(function (NotFoundHttpException $e) {
+            return $this->buildResponse(new NotFoundException());
+        });
+
+        $this->renderable(function (AccessDeniedHttpException $e, $request) {
+            return $this->shouldReturnJson($request, $e)
+                ? $this->buildResponse(new NotAuthorizedResourceException())
+                : redirect()->guest(route(RouteServiceProvider::UNAUTHORIZED));
+        });
+    }
+
+    private function buildResponse(CoreException $e): JsonResponse
+    {
+        if (config('app.debug')) {
+            $response = [
+                'message' => $e->getMessage(),
+                'errors' => $e->getErrors(),
+                'exception' => get_class($e),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'trace' => $e->getTrace(),
+            ];
+        } else {
+            $response = [
+                'message' => $e->getMessage(),
+                'errors' => $e->getErrors(),
+            ];
+        }
+
+        return response()->json($response, (int)$e->getCode());
+    }
+
+    protected function unauthenticated($request, LaravelAuthenticationException $e): JsonResponse|Response
+    {
+        return $this->shouldReturnJson($request, $e)
+            ? $this->buildResponse(new CoreAuthenticationException())
+            : redirect()->guest(route(RouteServiceProvider::LOGIN));
     }
 }
