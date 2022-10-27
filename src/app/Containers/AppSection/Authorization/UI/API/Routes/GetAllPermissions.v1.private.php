1459ab04b6b06dd4a6fd14a0e3b90141d3d98b58
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-27 05:29:33 +0300

    feat: Add GetAllPermissions action

---
+++ src/app/Containers/AppSection/Authorization/UI/API/Routes/GetAllPermissions.v1.private.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @apiGroup            Authorization
+ * @apiName             GetAllPermissions
+ *
+ * @api                 {GET} /v1/permissions Get All Permission
+ *
+ * @apiVersion          1.0.0
+ * @apiPermission       Authenticated ['permissions' => 'manage-roles', 'roles' => '']
+ *
+ * @apiHeader           {String} accept=application/json
+ * @apiHeader           {String} authorization=Bearer
+ *
+ * @apiUse              GeneralSuccessMultipleResponse
+ */
+
+use App\Containers\AppSection\Authorization\UI\API\Controllers\GetAllPermissionsController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('permissions', [GetAllPermissionsController::class, 'getAllPermissions'])
+    ->middleware(['auth:api']);
