71aef6958c769028e37fbe33ae48ebe9b663adf4
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-27 05:59:18 +0300

    fix: Add missing AttachPermissionsToRolesDto

---
+++ src/app/Containers/AppSection/Authorization/Data/Dto/AttachPermissionsToRolesDto.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Containers\AppSection\Authorization\Data\Dto;
+
+use App\Ship\Parents\Dto\Dto as ParentDto;
+use Spatie\DataTransferObject\Attributes\Strict;
+
+#[Strict]
+class AttachPermissionsToRolesDto extends ParentDto
+{
+    /**
+     * @var int[] Permissions ids
+     */
+    public array $permissions_ids;
+
+    /**
+     * @var int Role id
+     */
+    public int $role_id;
+}
