647523474fac84f1a824279ce7a422f72317e636
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-10-27 05:02:18 +0300

    feat: Add DeleteRole action

---
+++ src/app/Containers/AppSection/Authorization/UI/API/Routes/DeleteRole.v1.private.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @apiGroup            Authorization
+ * @apiName             DeleteRole
+ *
+ * @api                 {DELETE} /v1/roles/:id Delete a Role
+ * @apiDescription      Delete Role by ID
+ *
+ * @apiVersion          1.0.0
+ * @apiPermission       Authenticated ['permissions' => 'manage-roles', 'roles' => '']
+ *
+ * @apiHeader           {String} accept=application/json
+ * @apiHeader           {String} authorization=Bearer
+ *
+ * @apiParam            {String} id role id
+ *
+ * @apiSuccessExample   {json}       Success-Response:
+ * HTTP/1.1 202 OK
+ * {
+ * "message": "Role (manager) Deleted Successfully."
+ * }
+ */
+
+use App\Containers\AppSection\Authorization\UI\API\Controllers\DeleteRoleController;
+use Illuminate\Support\Facades\Route;
+
+Route::delete('roles/{id}', [DeleteRoleController::class, 'deleteRole'])
+    ->middleware(['auth:api']);
