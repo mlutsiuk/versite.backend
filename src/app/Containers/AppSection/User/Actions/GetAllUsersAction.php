9b1b9af7473cf8f5b3c437f3d55e507787ca6de6
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2022-11-22 03:23:10 +0200

    feat(User): Add GetAllUsers route

---
+++ src/app/Containers/AppSection/User/Actions/GetAllUsersAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Containers\AppSection\User\Actions;
+
+use Apiato\Core\Exceptions\CoreInternalErrorException;
+use App\Containers\AppSection\User\Tasks\GetAllUsersTask;
+use App\Ship\Parents\Actions\Action as ParentAction;
+use Prettus\Repository\Exceptions\RepositoryException;
+
+class GetAllUsersAction extends ParentAction
+{
+    /**
+     * @throws CoreInternalErrorException
+     * @throws RepositoryException
+     */
+    public function run(): mixed
+    {
+        return app(GetAllUsersTask::class)->run();
+    }
+}
---
+++ src/app/Containers/AppSection/User/UI/API/Routes/GetAllUsers.v1.private.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @apiGroup            User
+ * @apiName             GetAllUsers
+ *
+ * @api                 {GET} /v1/users Get All Users
+ * @apiDescription      Endpoint description here...
+ *
+ * @apiVersion          1.0.0
+ * @apiPermission       Authenticated ['permissions' => '', 'roles' => '']
+ *
+ * @apiHeader           {String} accept=application/json
+ * @apiHeader           {String} authorization=Bearer
+ *
+ * @apiUse              GeneralSuccessMultipleResponse
+ */
+
+use App\Containers\AppSection\User\UI\API\Controllers\GetAllUsersController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('users', [GetAllUsersController::class, 'getAllUsers'])
+    ->middleware(['auth:api']);
+
