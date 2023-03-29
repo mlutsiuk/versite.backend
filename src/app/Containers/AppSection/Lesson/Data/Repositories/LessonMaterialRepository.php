307129870643caf7d4d0735fb53a1164ff144a0b
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-29 17:24:24 +0300

    fix: Configure LessonMaterialRepository model

--- src/app/Containers/AppSection/Lesson/Data/Repositories/LessonMaterialRepository.php
+++ src/app/Containers/AppSection/Lesson/Data/Repositories/LessonMaterialRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\AppSection\Lesson\Data\Repositories;
 
+use App\Containers\AppSection\Lesson\Models\LessonMaterial;
 use App\Ship\Parents\Repositories\Repository as ParentRepository;
 
 class LessonMaterialRepository extends ParentRepository
@@ -13,4 +14,9 @@ class LessonMaterialRepository extends ParentRepository
         'id' => '=',
         // ...
     ];
+
+    public function model(): string
+    {
+        return LessonMaterial::class;
+    }
 }
