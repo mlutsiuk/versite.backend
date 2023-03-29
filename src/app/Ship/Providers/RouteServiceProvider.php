74ec446040bd8bb392911e6b7b059a4604795cf6
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-03-29 14:51:48 +0300

    fix: Rate limiting with cached routes

--- src/app/Ship/Providers/RouteServiceProvider.php
+++ src/app/Ship/Providers/RouteServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Ship\Providers;
 
 use App\Ship\Parents\Providers\RouteServiceProvider as ParentRouteServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\RateLimiter;
 
 class RouteServiceProvider extends ParentRouteServiceProvider
 {
@@ -32,4 +36,25 @@ class RouteServiceProvider extends ParentRouteServiceProvider
      * @var string
      */
     public const UNAUTHORIZED = 'unauthorized';
+
+    public function boot(): void
+    {
+        $this->configureRateLimiting();
+
+        parent::boot();
+    }
+
+    private function configureRateLimiting(): void
+    {
+        if (!Config::get('apiato.api.throttle.enabled')) {
+            return;
+        }
+
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinutes(
+                Config::get('apiato.api.throttle.expires'),
+                Config::get('apiato.api.throttle.attempts'))->by($request->user()?->id ?: $request->ip()
+            );
+        });
+    }
 }
