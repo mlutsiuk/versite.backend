6bcb73ad65048614ea7a3746d5e94595ff414578
Maksym Lutsiuk <63050099+mlutsiuk@users.noreply.github.com>
2023-04-07 00:43:59 +0300

    feat: Add relations to Course model

--- src/app/Containers/AppSection/Course/Models/Course.php
+++ src/app/Containers/AppSection/Course/Models/Course.php
@@ -2,9 +2,13 @@
 
 namespace App\Containers\AppSection\Course\Models;
 
+use App\Containers\AppSection\Lesson\Models\Lesson;
 use App\Containers\AppSection\Student\Models\Student;
+use App\Containers\AppSection\User\Models\User;
 use App\Ship\Parents\Models\Model as ParentModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Course extends ParentModel
 {
@@ -35,5 +39,35 @@ class Course extends ParentModel
     public function students(): HasMany
     {
         return $this->hasMany(Student::class, 'course_id','id');
+    }
+
+    /**
+     * Course lessons, one-to-many relations
+     *
+     * @return HasMany
+     */
+    public function lessons(): HasMany
+    {
+        return $this->hasMany(Lesson::class, 'course_id');
+    }
+
+    /**
+     * Course lessons, one-to-many relations
+     */
+    public function nextLesson(): HasOne
+    {
+        return $this->hasOne(Lesson::class, 'course_id')
+            ->where('date', '>', now())
+            ->orderBy('date');
+    }
+
+    /**
+     * Course author, many-to-one relations
+     *
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'author_id');
     }
 }
--- src/app/Containers/AppSection/Course/UI/API/Transformers/CourseTransformer.php
+++ src/app/Containers/AppSection/Course/UI/API/Transformers/CourseTransformer.php
@@ -3,9 +3,12 @@
 namespace App\Containers\AppSection\Course\UI\API\Transformers;
 
 use App\Containers\AppSection\Course\Models\Course;
+use App\Containers\AppSection\Lesson\UI\API\Transformers\LessonTransformer;
 use App\Containers\AppSection\Student\UI\API\Transformers\StudentTransformer;
+use App\Containers\AppSection\User\UI\API\Transformers\UserTransformer;
 use App\Ship\Parents\Transformers\Transformer as ParentTransformer;
 use League\Fractal\Resource\Collection;
+use League\Fractal\Resource\Item;
 
 class CourseTransformer extends ParentTransformer
 {
@@ -14,7 +17,10 @@ class CourseTransformer extends ParentTransformer
     ];
 
     protected array $availableIncludes = [
-        'students'
+        'students',
+        'lessons',
+        'next_lesson',
+        'author'
     ];
 
     public function transform(Course $course): array
@@ -41,5 +47,22 @@ class CourseTransformer extends ParentTransformer
     public function includeStudents(Course $course): Collection
     {
         return $this->collection($course->students, new StudentTransformer());
+    }
+
+    public function includeLessons(Course $course): Collection
+    {
+        return $this->collection($course->lessons, new LessonTransformer());
+    }
+
+    public function includeNextLesson(Course $course)
+    {
+        if($course->nextLesson) {
+            return $this->item($course->nextLesson, new LessonTransformer());
+        }
+    }
+
+    public function includeAuthor(Course $course): Item
+    {
+        return $this->item($course->author, new UserTransformer());
     }
 }
